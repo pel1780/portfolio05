4343bdc8adad5d94fad73ad4e4316b74048997d1
pel1780 <118698633+pel1780@users.noreply.github.com>
2022-12-28 17:48:24 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
 $(function () {
-    $('.con_img_slide').slick({});
-    $('.con_txt_slide').slick({});
+    $('.product_slide').on('init afterChange', function (event, slick, currentSlide) {
+        var current = $('.product_slide .slick-current');
+        current.addClass('on').siblings().removeClass('on');
+    });
+
+    $('.product_slide').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+    });
+
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop();
+        var conH = $('.mainContent').outerHeight(true);
+        var bgH = $('.con').height();
+        var osT = $('.con').eq(0).offset().top
+        console.log(sct, conH, bgH, $('.con').size(), osT);
+
+        var idx = 0;
+        for (i = 0; i < $('.con').size(); i++) {
+            if (sct > bgH * i + osT - 530) { idx = i }
+        }
+
+        $('.mainContent .tit>li').eq(idx).addClass('on').siblings().removeClass('on');
+        $('.mainContent .rib').stop().fadeOut();
+        $('.mainContent .rib').eq(idx).stop().fadeIn();
+    });
+
 })

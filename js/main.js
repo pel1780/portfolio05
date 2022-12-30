8e87dea201098ad2cb176b56c41016b4b8b74504
pel1780 <118698633+pel1780@users.noreply.github.com>
2022-12-30 15:53:02 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
+
 $(function () {
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop();
+        if (sct > 200) {
+            $('.header .to_top').fadeIn();
+        } else {
+            $('.header .to_top').fadeOut();
+        }
+    });
+    $('.header .to_top').on('click', function () {
+        $('html ,body').animate({ scrollTop: 0 }, 1000);
+    });
+    // to top 버튼작동
+
+
     $('.scroll_down down').on('click', function (event) {
         event.preventDefault();
-        $('html, body').animate({ scrollTop: $(this.hash).offset().top }, 500);
+        $('html, body').animate({ scrollTop: $(this.hash).offset().top }, 1000);
     })
-
-    // $('#toTop').on('click', function () {
-    //     $('html, body').animate({ scrollTop: 0 }, 1000)
-    // })
-
 
     $(window).on('scroll', function () {
         var sct = $(window).scrollTop();
@@ -17,18 +27,41 @@ $(function () {
             $('.scroll_down').fadeIn();
         }
     })
+    // scroll down 버튼 작동 & 사라졌다 생겼다 하게
+
 
     $('.product_slide').on('init afterChange', function (event, slick, currentSlide) {
         var current = $('.product_slide .slick-current');
         current.addClass('on').siblings().removeClass('on');
     });
 
-    $('.product_slide').slick({
+    var productSlide = $('.product_slide').slick({
         arrows: false,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 2500,
         fade: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
     });
+    // slide 작동
+
+    $('.controller .play button:first-child').on('click', function () {
+        $('.product_slide').slick('slickPlay');
+    })
+    $('.controller .play button:last-child').on('click', function () {
+        $('.product_slide').slick('slickPause');
+    })
+
+    $('.mainProduct .slide_bar').addClass('on');
+
+    productSlide.on("afterChange", function (e, s, current) {
+        $('.mainProduct .slide_bar').addClass('on');
+    });
+    productSlide.on("beforeChange", function (e, s, current) {
+        $(".slide_bar").removeClass("on");
+    })
+    // slide item event
+
 
     $(window).on('scroll', function () {
         var sct = $(window).scrollTop();
@@ -46,5 +79,6 @@ $(function () {
         $('.mainContent .rib').stop().fadeOut();
         $('.mainContent .rib').eq(idx).stop().fadeIn();
     });
+    // main content scroll event
 
 })
